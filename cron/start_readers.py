751096e557c11a5e0678dea0ee4567d4489d17d5
Radu Fericean <radu@fericean.ro>
2011-05-28 18:16:37 +0300

    protected against bad readers

--- cron/start_readers.py
+++ cron/start_readers.py
@@ -8,15 +8,17 @@ class_dict = {'rnd': RandomReader,
               'snmp': SNMPReader}
 
 for s in db(db.server.is_active == True).select():
-    reader = class_dict[s.reader](s.address, s.port)
-    info_array = reader.read()
-    db.reading.insert(server=s.id,
-                      cpu_utilization = info_array[0],
-                      mem_total = info_array[1],
-                      mem_used = info_array[2],
-                      swap_total = info_array[3],
-                      swap_used = info_array[4],
-                      )
-    db.commit()
+    try:
+        reader = class_dict[s.reader](s.address, s.port)
+        info_array = reader.read()
+        db.reading.insert(server=s.id,
+                          cpu_utilization = info_array[0],
+                          mem_total = info_array[1],
+                          mem_used = info_array[2],
+                          swap_total = info_array[3],
+                          swap_used = info_array[4],
+                          )
+    except: pass
+db.commit()
 
     
