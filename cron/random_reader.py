cc35a988d81c8ff3e9495daa7ecdc0aa199f8d56
Radu Fericean <radu@fericean.ro>
2011-05-29 12:53:17 +0300

    even better random reader

--- cron/random_reader.py
+++ cron/random_reader.py
@@ -8,8 +8,8 @@ class RandomReader(object):
     def read(self):
         info_array = []
         info_array.append(random.randint(1,100)) #cpu
-        info_array.append(random.randint(0,8000)) #mem_total
+        info_array.append(8000) #mem_total
         info_array.append(random.randint(0,8000)) #mem_used
-        info_array.append(random.randint(0,8000)) #swap_total
+        info_array.append(4000) #swap_total
         info_array.append(random.randint(0,8000)) #swap_used
         return info_array
