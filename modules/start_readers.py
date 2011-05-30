8a5e1db96d270d05f1fab513fcbff6cafe2bad57
Radu Fericean <radu@fericean.ro>
2011-05-30 16:16:43 +0300

    the server still has to be s.id

--- modules/start_readers.py
+++ modules/start_readers.py
@@ -11,14 +11,10 @@ for s in db(db.server.is_active == True).select():
     try:
         reader = class_dict[s.reader](s.address, s.port)
         info_array = reader.read()
-        db.reading.insert(server=s.id,
-                          cpu_utilization = info_array[0],
-                          mem_total = info_array[1],
-                          mem_used = info_array[2],
-                          swap_total = info_array[3],
-                          swap_used = info_array[4],
-                          )
-    except: pass
+        db.reading.insert(server = s.id, cpu_utilization = info_array[0], mem_total = info_array[1], mem_used = info_array[2], swap_total = info_array[3], swap_used = info_array[4])
+    except:
+        db.reading.insert(server = s.id, cpu_utilization = -1, mem_total = -1, mem_used = -1, swap_total = -1, swap_used = -1)
+        
 db.commit()
 
     
