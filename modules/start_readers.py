7ea9c2da41e45ed726a65cfaac04f66a588ce55b
Radu Fericean <radu@fericean.ro>
2011-06-04 20:41:55 +0300

    normal import was not working for snmp

--- modules/start_readers.py
+++ modules/start_readers.py
@@ -1,14 +1,15 @@
 #!/bin/env python
 from random_reader import RandomReader
 from ssh_reader import SSHReader
-from snmp_reader import SNMPReader
+snmp_reader = local_import('snmp_reader')
 
 class_dict = {'rnd': RandomReader,
               'ssh': SSHReader,
-              'snmp': SNMPReader}
+              'snmp': snmp_reader.SNMPReader}
 
 for s in db(db.server.is_active == True).select():
     try:
+        print s.reader
         reader = class_dict[s.reader](s.address, s.port)
         info_array = reader.read()
         db.reading.insert(server = s.id, cpu_utilization = info_array[0], mem_total = info_array[1], mem_used = info_array[2], swap_total = info_array[3], swap_used = info_array[4])
