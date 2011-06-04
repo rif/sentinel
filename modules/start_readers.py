e4b6d922366b656a70a9aead37fb111b20666b04
Radu Fericean <radu@fericean.ro>
2011-06-04 20:56:12 +0300

    set ssh timeout 10 seconds

--- modules/start_readers.py
+++ modules/start_readers.py
@@ -9,7 +9,6 @@ class_dict = {'rnd': RandomReader,
 
 for s in db(db.server.is_active == True).select():
     try:
-        print s.reader
         reader = class_dict[s.reader](s.address, s.port)
         info_array = reader.read()
         db.reading.insert(server = s.id, cpu_utilization = info_array[0], mem_total = info_array[1], mem_used = info_array[2], swap_total = info_array[3], swap_used = info_array[4])
